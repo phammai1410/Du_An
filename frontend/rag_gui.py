2381a7329443aec322bb56da84dc238058229a0a
Mai <maipham141102004@gmail.com>
2025-10-19 21:56:47 +0700

    add GUI, update ignore, lib

--- frontend/rag_gui.py
+++ frontend/rag_gui.py
@@ -12,7 +12,7 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 
 # -----------------------------
 # Paths & Config
